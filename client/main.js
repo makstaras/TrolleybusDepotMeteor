71aeebac513a971c5c5b6b6509b938daa7e94821
makstaras <makstaras@ukr.net>
2015-03-24 16:54:19 +0200

    First commit

---
+++ client/main.js
@@ -0,0 +1,47 @@
+Template.blog.events({
+	'submit #blogForm':function(e){
+		e.preventDefault(); 
+		var title = $('#blogTitle').val();
+		var body = $('#blogBody').val();
+		if(title.length && body.length){
+			Meteor.call('submitPost',title,body)
+		}
+	}
+})
+
+Template.listblogs.blogs = function(){
+	return Blogs.find(); 
+}
+
+Template.main.rendered = function(){
+	var calendar = $('#calendar').fullCalendar({	
+	})
+}
+
+Router.route('post',{
+	path: '/post',	
+});
+
+Router.route('calendar',{
+	path: '/calendar',	
+});
+
+Router.route('home',{
+		
+	path:'/:limit?',
+	template: 'home',
+	data: function() {
+		var news = [{
+			Project:'Information system on C++',
+			Description:'with files'
+			},{
+			Project:'Information system on C#',
+			Description:'DB - MySQL'
+			},{
+			Project:'Internet shop on PHP',
+			Description:'pure php'
+			}		
+		]
+		return {news: news.slice(0,this.params.limit)};
+	}
+});
